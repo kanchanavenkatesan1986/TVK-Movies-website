caeb8bab85675c359bbb77abb68b996bd55b60bb
allanimetamil <teenteenagepassanga@gmail.com>
2026-03-09 03:45:33 +0530

    bjn

--- dist/Bybass.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    const params = new URLSearchParams(window.location.search);
-    const next = params.get("unlock");
-
-    if (next) {
-        // Normal browser fallback
-        window.location.replace(next);
-    }
-})();
