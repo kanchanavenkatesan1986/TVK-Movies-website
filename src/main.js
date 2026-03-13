e5be1c34143cd09739beec3b91633f7f36e9dd61
allanimetamil <teenteenagepassanga@gmail.com>
2026-03-14 02:13:18 +0530

    dcsavgf

--- src/main.js
+++ src/main.js
@@ -11,10 +11,10 @@ function closeNav(){
     function shareApp() {
 
     var message =
-        "Watch Free Tamil Movies 🎬\n\n" +
+        "TVK Movies-Tamil Cinemas 🎬\n\n" +
+        "Watch Free Tamil & Hollywood Movies Watch Free in Tamil 🎬\n\n" +
         "Download our app and enjoy unlimited entertainment!\n\n" +
-        "Play Store:\nhttps://play.google.com/store/apps/details?id=com.vipmovies.tvkmovies&pcampaignid=web_share\n\n" +
-        "Direct APK:\nhttps://yourwebsite.com/app.apk";
+        "Play Store:\nhttps://play.google.com/store/apps/details?id=com.vipmovies.tvkmovies&pcampaignid=web_share";
 
     if (navigator.share) {
 
